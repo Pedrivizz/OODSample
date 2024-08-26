5d44d9bbffad736210be5ae44228780b796e3422
Pedro Guia <u202212010@upc.edu.pe>
2024-08-25 19:16:59 -0500

    feat(sales-order): added sales order item entity.

---
+++ OODSample/Sales/Domain/Model/Aggregates/SalesOrderItems.cs
@@ -0,0 +1,51 @@
+namespace OODSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrderItem
+{
+    public SalesOrderItem(Guid salesOrderId, int productId, int quantity, double unitPrice)
+    {
+        if (salesOrderId == Guid.Empty)
+        {
+            throw new ArgumentException("Sales Order ID is required");
+        }
+
+        if (ProductId <= 0)
+        {
+            throw new ArgumentException("Product ID is required and must be greater than zero");
+        }
+
+        if (quantity <= 0)
+        {
+            throw new ArgumentException("Quantity must be greater than zero");
+        }
+
+        if (unitPrice <= 0)
+        {
+            throw new AggregateException("Unit Price must be greater than zero");
+        }
+        SalesOrderId = salesOrderId;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
+    // Public section
+    
+    
+    public Guid Id { get; private set; } = GenerateOrderItemId();
+    
+    public Guid SalesOrderId { get; }
+
+    public int ProductId { get; }
+
+    public int Quantity { get; }
+
+    public double UnitPrice { get; }
+
+    public double CalculateItemPrice() => Quantity * UnitPrice;
+
+    // Private section
+    private static Guid GenerateOrderItemId()
+    {
+        return Guid.NewGuid();
+    }
+}
